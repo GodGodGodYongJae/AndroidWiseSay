5fe0ccae7132003df075f9e486f5b8a93ed3c33d
zhaqj12 <zhaqj12@naver.com>
2022-07-06 17:33:07 +0900

    DoubleClick Back Button

--- app/src/main/java/com/kyeonghoon/customfont/MainActivity.kt
+++ app/src/main/java/com/kyeonghoon/customfont/MainActivity.kt
@@ -2,9 +2,14 @@ package com.kyeonghoon.customfont
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var isDouble = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,5 +25,19 @@ class MainActivity : AppCompatActivity() {
         val listview = findViewById<ListView>(R.id.mainListView)
         val listadapter = ListViewAdapter(list_item)
         listview.adapter = listadapter
+    }
+
+    override fun onBackPressed() {
+        if(isDouble == true)
+        {
+            //finish()
+            super.onBackPressed()
+        }
+        isDouble = true
+        Toast.makeText(this,"종료하실려면 한번 더 눌러주세요!.",Toast.LENGTH_LONG).show()
+        Handler().postDelayed(Runnable {
+            isDouble = false
+        },2000)
+
     }
 }
