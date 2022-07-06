8de1aca0cf6814eba6c6299d395682d61ad9e782
zhaqj12 <zhaqj12@naver.com>
2022-07-06 17:07:23 +0900

    ListView , ListViewModel 구현, ? !! 이게 뭘까

---
+++ app/src/main/java/com/kyeonghoon/customfont/ListViewModel.kt
@@ -0,0 +1,6 @@
+package com.kyeonghoon.customfont
+
+data class ListViewModel (
+    var title : String = "",
+    var content : String = ""
+)
--- app/src/main/java/com/kyeonghoon/customfont/MainActivity.kt
+++ app/src/main/java/com/kyeonghoon/customfont/MainActivity.kt
@@ -2,10 +2,23 @@ package com.kyeonghoon.customfont
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list_item = mutableListOf<ListViewModel>()
+
+
+        list_item.add(ListViewModel("제목1","나는 내용1"))
+        list_item.add(ListViewModel("제목2","나는 내용2"))
+        list_item.add(ListViewModel("제목3","나는 내용3"))
+        list_item.add(ListViewModel("제목4","나는 내용4"))
+
+        val listview = findViewById<ListView>(R.id.mainListView)
+        val listadapter = ListViewAdapter(list_item)
+        listview.adapter = listadapter
     }
 }
