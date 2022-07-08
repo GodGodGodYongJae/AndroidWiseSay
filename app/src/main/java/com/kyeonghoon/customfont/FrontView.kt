95ad159b830c7c918683dd34d0fffa54ddc7d4e8
zhaqj12 <zhaqj12@naver.com>
2022-07-08 10:29:34 +0900

    List View

--- app/src/main/java/com/kyeonghoon/customfont/FrontView.kt
+++ app/src/main/java/com/kyeonghoon/customfont/FrontView.kt
@@ -15,6 +15,18 @@ class FrontView : AppCompatActivity () {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.frontview)
 
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.")
+        sentenceList.add("명언2.")
+        sentenceList.add("명언3.")
+        sentenceList.add("명언4.")
+        sentenceList.add("명언5.")
+        sentenceList.add("명언6.")
+        sentenceList.add("명언7.")
+        sentenceList.add("명언8.")
+
+
+
 
         val binding = DataBindingUtil.setContentView<FrontviewBinding>(this, R.layout.frontview)
         binding.showAllSentenceBtn.setOnClickListener {
@@ -22,7 +34,7 @@ class FrontView : AppCompatActivity () {
             startActivity(intent)
         }
 
-
+        binding.goodword.text =   sentenceList.random()
     }
 }
 
